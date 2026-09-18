6d7e6516e16aaecf50c350d7ad25045368caedd4
mostakinahmed <mostakin4556@gmail.com>
2026-09-18 18:06:35 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./page/Home";
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
